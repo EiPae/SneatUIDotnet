204fd15d75d794337e2f417d7bac3dec18501336
EiPae <eithaepomyanmar05033@gmail.com>
2024-06-27 22:07:57 +0900

    Adding Account Controller with 3 view pages

---
+++ SneatUIDotnet/Controllers/AccountSettingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SneatUIDotnet.Controllers
+{
+    public class AccountSettingsController : Controller
+    {
+        public IActionResult Account()
+        {
+            return View();
+        }
+        public IActionResult Notifications()
+        {
+            return View();
+        }
+        public IActionResult Connections()
+        {
+            return View();
+        }
+    }
+}
